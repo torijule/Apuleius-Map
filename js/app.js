0f171e0eb5ac135fead0ff54c172b29ed75c740e
torijule <torijule@gmail.com>
2016-05-30 16:11:44 -0700

    Setting up files

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//Victoria Wellington
+//App to establish the Pliedes Map using Google Maps API
+
+"use strict";
+
+$(document).ready(function(){
+	
+$(document).ready(function(){
+	 var mapOptions = {
+	          center: { lat: 47.6, lng: -122.3},
+	          zoom: 12
+	 };
+	var map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+	var info = new google.maps.InfoWindow();
+	var infoWindow = new google.maps.InfoWindow();
+	
+	 );
+
+}) ;
+	
+	
+}
