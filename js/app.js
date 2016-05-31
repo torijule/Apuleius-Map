e75fca0df91ab687c8d05a08117f279856ffa8e8
torijule <torijule@gmail.com>
2016-05-30 19:58:09 -0700

    Wooo!  Displays Pelagios Map!!!!
    Centered on Rome.  Ridiculously slow functionality.
    
    May need to test with other sizes for speed.  But it loads and works!!!!
    
    todo: Create JSON objects for places, add markers.

--- js/app.js
+++ js/app.js
@@ -2,20 +2,54 @@
 //App to establish the Pliedes Map using Google Maps API
 
 "use strict";
-	
-// Victoria Wellington
-//app to get pictures of seattle city traffic locations and georeference them with google maps
 
-"use strict";
-
+var zoom, coord;
 
 $(document).ready(function(){
-	 var mapOptions = {
-	          center: { lat: 47.6, lng: -122.3},
-	          zoom: 12
-	 };
-	var map = new google.maps.Map(document.getElementById('map'),
-            mapOptions);
+	//var rome = new google.maps.LatLng(41.8922,12.4852);
+	//coord = rome;
+	var options = { 
+	  zoom: 7,
+	  center: new google.maps.LatLng(41.8922,12.4852)
+		/*//disableDefaultUI: true,
+		zoomControl: true,
+		center: {lat: -33, lng: 151},
+		//mapTypeId: google.maps.MapTypeId.ROADMAP,
+		zoom: 7,
+		//zoomControlOptions: {
+		//	style: google.maps.ZoomControlStyle.SMALL
+		//} */
+	}
+	
+	
+	var map = new google.maps.Map(document.getElementById("map"), options);
+
+	
+	var ancientMap = new google.maps.ImageMapType({
+		getTileUrl: function(coord, zoom){
+			//return 'http://pelagios.dme.ait.ac.at/tilesets/imperium/{z}/{x}/{y}.png';
+			//return 'http://pelagios.dme.ait.ac.at/tilesets/imperium/7/66/45.png';
+			return "http://pelagios.dme.ait.ac.at/tilesets/imperium/" + zoom + "/" + coord.x + "/" + coord.y + ".png";
+		},
+		tileSize: new google.maps.Size(256, 256),
+		isPng:true,
+		minZoom: 3,
+		maxZoom: 11,
+		name: "Imperium"
+	});
+	
+	
+	//setCenter(167717);
+	map.mapTypes.set('imperium', ancientMap);
+	map.setMapTypeId('imperium');
+	//new google.maps.LatLng(41.923, 12.513)
+	//map.setCenter(new google.maps.LatLng(42,12));
+	//map.overlayMapTypes.push(imageMapType);
+	map.panTo(new google.maps.LatLng(41.8922,12.4852))
+
+
+
 	var info = new google.maps.InfoWindow();
 	var infoWindow = new google.maps.InfoWindow();
+	
 }) ;
